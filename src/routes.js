8033db9a620813e5fdf9b54f8707d1f7c1010bb3
Arthur Wagenknecht <oarthurdev@gmail.com>
2020-07-07 21:11:27 -0300

    Alterações q esqueci de commitar

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const express = require('express')
 
-const CompanyController = require('./controllers/CompanyController')
 const UserController = require('./controllers/UserController')
 
 const routes = express.Router()
@@ -9,8 +8,8 @@ routes.post('/auth/login', UserController.login)
 routes.post('/auth/register', UserController.register)
 routes.post('/auth/logout', UserController.logout)
 
-routes.get('/company', UserController.auth, CompanyController.index)
+// routes.get('/company', UserController.auth, CompanyController.index)
 
-routes.post('/company/update', UserController.auth, CompanyController.update)
+// routes.post('/company/update', UserController.auth, CompanyController.update)
 
 module.exports = routes
